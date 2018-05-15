443ba2c68fcfc149d37d6d98c83a72837f4fff29
novalid <noelle.schenk@students.unibe.ch>
2018-05-15 10:21:07 +0200

    added files

---
+++ n_first_test.py
@@ -0,0 +1,91 @@
+############################
+# 1 - loading data
+# 2 - idea 1
+#
+
+import sys
+import matplotlib
+matplotlib.rcParams['image.cmap'] = 'gray'
+matplotlib.rcParams['image.interpolation'] = 'nearest'
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import numpy as np
+from skimage import feature, color
+from scipy.signal import convolve2d
+import datetime
+import time
+import os
+from PIL import Image
+import csv
+
+# - 1 - ##################
+# Load images from Folder
+#############
+
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = plt.imread(os.path.join(folder,filename))
+        if img is not None:
+            images.append(img)
+    return images
+
+imgs = load_images_from_folder("./group_project/h4group/project_data/a")
+#plt.imshow(imgs[2])
+test = np.copy(imgs[1])
+tesw = np.copy(imgs[8][:,:,1])
+
+# run code from homework 2!
+
+# - 2 - ##################
+# Idea 1
+#############
+# idea :  don't detect the tool itself, but the deviation from a clear round shape.
+# approach : ?
+
+# ------------------ testing around
+# where are high differences in illumination? Find the border pixels.
+# make image lighter, enhance contrast
+
+
+np.min(imgs[1])
+np.max(imgs[1])
+np.mean(imgs[1])
+np.median(imgs[1])
+
+
+test[test > np.mean(imgs[1])] = 1
+plt.imshow(test[:,:,1])
+plt.imshow(tesw)
+
+plt.hist(tesw.ravel(), bins=256, range=(0.0, 1.0), fc='k', ec='k')
+tesw[tesw < 0.3] = 1
+tesw = tesw + 0.2
+tesw[tesw > 1] = 1
+np.max(tesw)
+
+
+
+# - 3 - ##################
+# Idea 2
+#############
+# APPLY DIFFERENCE OF GAUSSIAN
+DoG_of_img = DoG(tesw, 10, 25, 5)
+plt.imshow(DoG_of_img)
+
+# - 4 - ##################
+# Idea 3
+#############
+# subtract a smoothed image to get the shape much better
+img_filtered_1 = gconv(tesw, 70, 41)
+np.shape(img_filtered_1)
+np.shape(tesw0)
+tesw0 = np.pad(tesw, 20, mode="constant")
+plt.imshow(img_filtered_1)
+plt.imshow(tesw0)
+gagi = tesw0 - img_filtered_1
+plt.imshow(gagi)
+
+# next thing to do : run a detector of a "stab" here! a filter.
+# maybe detection on this works best.
+
